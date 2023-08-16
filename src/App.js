74f737914eace84f57e3bb78f99549b1f60577a0
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-08-16 13:12:00 -0500

    initial setup of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      {/* Switch will only render the first route that matches the path. */}
+      <Switch>
+        <Route path="/" exact> <Users /> </Route>
+        <Route path="/places/new" exact> <NewPlace /> </Route>
+
+        {/* if none of the other paths are valid redirect will take you to whatever url youve specified for it. in this case the "/" path  */}
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
