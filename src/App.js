42a6fb552ec275cdcc73b728537d0ebdf037fe01
Adam Brannon <adambrannon@Adams-MacBook-Pro.local>
2023-08-24 10:56:03 -0500

    added header and nav components folder with some structure code

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 function App() {
   return (
     <Router>
-      {/* Switch will only render the first route that matches the path. */}
-      <Switch>
-        <Route path="/" exact> <Users /> </Route>
-        <Route path="/places/new" exact> <NewPlace /> </Route>
 
-        {/* if none of the other paths are valid redirect will take you to whatever url youve specified for it. in this case the "/" path  */}
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+
+      <main>
+        {/* Switch will only render the first route that matches the path. */}
+        <Switch>
+          <Route path="/" exact> <Users /> </Route>
+          <Route path="/places/new" exact> <NewPlace /> </Route>
+
+          {/* if none of the other paths are valid redirect will take you to whatever url youve specified for it. in this case the "/" path  */}
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   )
 }
